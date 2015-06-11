23983dd87d934542de0bb4e3d3e07a9ec136091d
Enifs <d.romanovskis@gmail.com>
2015-06-11 22:25:15 +0300

    Added javadoc comments to ECP and Event classes.

--- src/events/Event.java
+++ src/events/Event.java
@@ -6,37 +6,73 @@ package events;
 import java.util.HashSet;
 
 
+/**
+ * This is the base class for every event in this system.
+ * Class extending this one should have following line in its constructor -
+ * this.setEventHandler(new EventHandler());
+ */
 public abstract class Event
 {
+	/**
+	 * This method must be called to make an event happen.
+	 */
 	public void fire()
 	{
 		EventControlPanel.fireEvent(this);
 	}
 
+
+	/**
+	 * This method gets the event handler bound to this event class.
+	 * @return An event handler object bound to this class.
+	 */
 	protected EventHandler getEventHandler()
 	{
 		return eventHandler;
 	}
 
 
+	/**
+	 * This method must be called in events constructor for any event handling to happen.
+	 * @param eventHandler
+	 */
 	public void setEventHandler(EventHandler eventHandler)
 	{
 		this.eventHandler = eventHandler;
 	}
 
-	public static void disable(Class c)
+
+	/**
+	 * This method is used to block events of the given class so they would not execute.
+	 *
+	 * @param eventClass Event class to disable.
+	 */
+	public static void disable(Class eventClass)
 	{
-		Event.disabledEvents.add(c);
+		Event.disabledEvents.add(eventClass);
 	}
 
-	public static void enable(Class c)
+
+	/**
+	 * This method enables previously disabled event.
+	 *
+	 * @param eventClass Event class to get enabled.
+	 */
+	public static void enable(Class eventClass)
 	{
-		Event.disabledEvents.remove(c);
+		Event.disabledEvents.remove(eventClass);
 	}
 
-	public static boolean isEnabled(Class c)
+
+	/**
+	 * This method checks if given event class is enabled or no.
+	 *
+	 * @param eventClass Class to have its status checked.
+	 * @return True, if given event class is allowed to fire events.
+	 */
+	public static boolean isEnabled(Class eventClass)
 	{
-		return !Event.disabledEvents.contains(c);
+		return !Event.disabledEvents.contains(eventClass);
 	}
 
 	private EventHandler eventHandler = new EmptyEventHandler();
--- src/events/EventControlPanel.java
+++ src/events/EventControlPanel.java
@@ -7,14 +7,26 @@ import events.regularevents.RegularEvent;
 import events.regularevents.RegularEventDispatcher;
 
 
+/**
+ * This is main control class. The system is initialized for event handling by calling the default
+ * constructor of this class. There is no need for a user to maintain an instance of this class.
+ */
 public class EventControlPanel
 {
+	/**
+	 * Default constructor. Call this method to initialize event handling system.
+	 * IF not initialized, you will face some null pointer exceptions.
+	 */
 	public EventControlPanel()
 	{
 		this.startEventHandling();
 	}
 
 
+	/**
+	 * Manual switch for starting up the event handling system. Use this if you have stopped event
+	 * handling manually at some point in your code.
+	 */
 	public void startEventHandling()
 	{
 		EventControlPanel.events = new EventQueue();
@@ -30,6 +42,10 @@ public class EventControlPanel
 		EventControlPanel.events.clear();
 	}
 
+
+	/**
+	 * Off switch for this system.
+	 */
 	public void stopEventHandling()
 	{
 		// todo: stop is deprecated maybe a better solutions exist.
@@ -42,27 +58,52 @@ public class EventControlPanel
 		EventControlPanel.events.clear();
 	}
 
+
+	/**
+	 * This method registers a regular event in this system.
+	 * @param event The event to register in the system.
+	 */
 	public static void registerRegularEvent(RegularEvent event)
 	{
 		EventControlPanel.dispatcher.registerEvent(event);
 	}
 
+
+	/**
+	 * This method removes a regular event from this system.
+	 * @param event The event to remove.
+	 */
 	public static void unRegisterRegularEvent(RegularEvent event)
 	{
 		EventControlPanel.dispatcher.unregisterEvent(event);
 	}
 
 
+	/**
+	 * This method queues ap an event to be executed as soon as possible.
+	 * @param event Event to queue up for execution.
+	 */
 	public static void fireEvent(Event event)
 	{
 		EventControlPanel.events.offer(event);
 	}
 
+
+	/**
+	 * This method says if there are any events currently queued up for execution.
+	 *
+	 * @return True, if there are events in the event queue;
+	 */
 	public static boolean hasEvents()
 	{
 		return !EventControlPanel.events.isEmpty();
 	}
 
+
+	/**
+	 * This method fetches next Event from the event queue. It might return null.
+	 * @return Event retrieved from the event queue.
+	 */
 	public static Event nextEvent()
 	{
 		return EventControlPanel.events.poll();
